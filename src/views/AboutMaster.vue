a563e3d751da1dac880b9845cec65bbba311236e
Aeshe <syslichek@mail.ru>
2023-11-08 10:28:44 +0300

    Добавлен axios запрос на получение конктретного мастера по id из БД

--- src/views/AboutMaster.vue
+++ src/views/AboutMaster.vue
@@ -1,40 +1,56 @@
 <template>
-  <section class="aboutMaster container">
-    <div class="photo">
-      <img :src="require(`@/assets/img/${getMaster.photo}`)" alt="our_master" />
-    </div>
-    <div class="aboutMaster__content">
-      <p class="aboutMaster__text">{{ getMaster.name }}</p>
-      <p class="aboutMaster__text">{{ getMaster.position }}</p>
-      <p class="aboutMaster__text">
-        {{ getMaster.aboutText }}
-      </p>
-      <router-link
-        :to="`/appointment/master/${getMaster.id}`"
-        class="subscribeBtn"
-        >Записаться к мастеру</router-link
-      >
+  <section class="container">
+    <div v-if="loading">loading</div>
+    <div v-if="getMaster" class="aboutMaster">
+      <div class="photo">
+        <img
+          :src="require(`@/assets/img/${getMaster.photo}`)"
+          alt="our_master"
+        />
+      </div>
+      <div class="aboutMaster__content">
+        <p class="aboutMaster__text">{{ getMaster.masterName }}</p>
+        <p class="aboutMaster__text">{{ getMaster.position }}</p>
+        <p class="aboutMaster__text">
+          {{ getMaster.aboutText }}
+        </p>
+        <router-link
+          :to="`/appointment/master/${getMaster.id}`"
+          class="subscribeBtn"
+          >Записаться к мастеру</router-link
+        >
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import axios from "axios";
 import { mapGetters } from "vuex";
 // import store from "@/store";
 export default {
   name: "AboutMaster",
 
   data() {
-    return {};
+    return {
+      masterID: +this.$route.params.masterID,
+      loading: true,
+      getMaster: null,
+    };
   },
 
   computed: {
-    ...mapGetters(["GET_MASTER_FOR_ID"]),
-    getMaster() {
-      const masterID = +this.$route.params.masterID;
-      //   return store.getters.GET_MASTER_FOR_ID(masterID);
-      return this.GET_MASTER_FOR_ID(masterID);
-    },
+    ...mapGetters(["getServerUrl"]),
+  },
+  created() {
+    axios
+      .get(
+        `${this.getServerUrl}/masters/search/getMasterById?id=${this.masterID}`
+      )
+      .then((res) => {
+        this.getMaster = res.data;
+        this.loading = false;
+      });
   },
 };
 </script>
--- src/views/AppointmentPages/MakeAppointment.vue
+++ src/views/AppointmentPages/MakeAppointment.vue
@@ -1,23 +1,70 @@
 <template>
   <div>
-    <p>Ьастер 1(или любой мастер)</p>
-    <p>Услуга 1</p>
+    <p>{{ getSelectMasterName }}</p>
+    <p>{{ getSelectService.title }}</p>
+    <p>Длительность: {{ getSelectService.duration }} мин.</p>
+    <p>Цена: {{ getSelectService.price }} руб.</p>
     <p>Выберите дату время</p>
+    <input type="date" id="dateAppointment" v-model="selectedData" />
+    <p>{{ selectedData }}</p>
+    
     <button>Записаться</button>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+// import axios from "axios";
 export default {
   name: "MakeAppointment",
 
   data() {
-    return {};
+    return {
+      masterId: +this.$route.params.idMaster,
+      serviceId: +this.$route.params.idService,
+      selectedData: null,
+    };
   },
 
-  mounted() {},
+  // mounted() {
+  //   axios.get(`${this.getServerUrl}/services?page=0&size=25`).then((res) => {
+  //     const data = [];
+  //     res.data._embedded.services.forEach((element) => {
+  //       data.push({
+  //         id: element.id,
+  //         title: element.title,
+  //         groupServiceId: element.groupServiceId,
+  //         duration: element.duration,
+  //         price: element.price,
+  //       });
+  //     });
+  //     this.SET_SERVICES(data);
+  //   });
+  // },
 
-  methods: {},
+  computed: {
+    ...mapGetters(["GET_MASTERS", "getServerUrl", "GET_SERVICES", "GET_SHEDULE"]),
+
+    getSelectMasterName() {
+      if (this.masterId) {
+        return this.GET_MASTERS.find((el) => el.id === this.masterId).name;
+      } else {
+        return "Любой мастер";
+      }
+    },
+    getSelectService() {
+      return this.GET_SERVICES.find((el) => el.id === this.serviceId);
+    },
+    // getFreeTime(){
+    //   const freeTime = [];
+    //   const sheduleMasterOfSelectDay = this.GET_SHEDULE.filter(el => el.date_appointment.localeCompare(this.selectedData));
+      
+    //   return freeTime;
+    // }
+  },
+  methods: {
+    ...mapMutations(["SET_MASTERS"]),
+  },
 };
 </script>
 
